f2280d75d750f0d452a386a9bbf4e0249831fadc
RemediaVC <jtartuymaesnand@gmail.com>
2026-01-16 23:37:57 -0800

    First instance of RPS Logic
    Didn't have a whole lot of time today, spent about 40 minutes. Added basic handling of the main RPS system. Trying to make it as scalable as possible with components. Using Game master to handle most of the connections between, so nodes don't have to communicate with anything else.
    +added new CPU scene with a script 
    +added signals that communicates to game master node
    +added CPU logic that waits for a submit from the user and picks randomly between RPS. (Might weight the decision later)
    +changed variable names to avoid variable overlap
    +general format changes to the button
    +added canvas layer w/ upgrade scene on stand by so that the upgrade scene can float above the battle scene
    +using unique names which is new to gdscript 4.x

--- Scripts/cpu.gd
+++ Scripts/cpu.gd
@@ -1,29 +1,28 @@
 extends MarginContainer
 
 var selected: Object = null
+@onready var list_of_available_RPS = []
 
 func _ready() -> void:
+	randomize()
 	var gm = %GameMaster
 	
 	var rock_instance = gm.RockComponent.instantiate()
 	var paper_instance = gm.PaperComponent.instantiate()
 	var scissors_instance = gm.ScissorsComponent.instantiate()
 	
-	var list_of_available_RPS = []
+	gm.connect("player_submitted", _on_GM_request)
 	
 	get_child(0).add_child(rock_instance)
 	get_child(0).add_child(paper_instance)
 	get_child(0).add_child(scissors_instance)
 	
-	#List for easy access to components for upgrades/changes, each binded
 	for child in get_child(0).get_children():
 		list_of_available_RPS.append(child)
-		list_of_available_RPS.back().pressed.connect(_on_RPS_select.bind(child))
-	
-
-	
-	
-func _on_RPS_select(childObj):
-	selected = childObj
-
+		list_of_available_RPS.back().disabled = true
+		
+		
+func _on_GM_request():
+	selected = list_of_available_RPS[randi() % list_of_available_RPS.size()]
+	print(selected)
 	
--- Scripts/game_master.gd
+++ Scripts/game_master.gd
@@ -1,19 +1,24 @@
 extends Node
-
+signal player_submitted
 #All component preload
 var RockComponent = preload("res://Scenes/RPSCompnents/rockcomponent.tscn")
 var PaperComponent = preload("res://Scenes/RPSCompnents/papercomponent.tscn")
 var ScissorsComponent = preload("res://Scenes/RPSCompnents/scissorscomponent.tscn")
 var p1_submitted: Object = null
+var cpu_submitted: Object = null
 
+@onready var cpu = %CPU
+@onready var p1 = %Player
 func _ready() -> void:
-	var p1 = %Player
 	%Submit.pressed.connect(_on_submit_pressed.bind(p1))
 	
 	
-func _on_submit_pressed(p1: Object):
-	p1_submitted = p1.selected
-	print(p1_submitted)
+func _on_submit_pressed(p1obj: Object):
+	p1_submitted = p1obj.selected
+	print("Player: " + str(p1_submitted))
+	CPU_wake_up()
 
-
-	
+func CPU_wake_up():
+	emit_signal("player_submitted")
+	cpu_submitted = cpu.selected
+	print("CPU: " + str(cpu_submitted))
