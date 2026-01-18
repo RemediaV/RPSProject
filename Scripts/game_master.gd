3d1451bc79e818a76541d90aa31a42c95d6c2583
RemediaVC <jtartuymaesnand@gmail.com>
2026-01-17 23:24:04 -0800

    Reworked RPS Mechanic
    Reworked all of the scripts today, removed unique names because they are hierarchy dependent and I needed more flexibility. Using 2 global groups and calling functions that way. 
    -removed unique names 
    -removed cpu and player scripts
    +added rps_logic script which is used by both cpu and player instead (the code was previously very similar)
    +added groups GM and All_Nodes_Caller, all nodes can now ref GM(GameMaster) and GameMaster can call functions on every other grouped node from ANYWHERE.
    +formatted scene to more match my final vision
    +added labels to CPU and Player
    +added a failsafe if player submits nothing
    // made the win meters red, formatted them, and a single scene for them. 
    // changed the selected variable in rps_logic to a string of the selected component's name for easier dictionary look up.

--- Scripts/game_master.gd
+++ Scripts/game_master.gd
@@ -1,24 +1,54 @@
 extends Node
-signal player_submitted
+
 #All component preload
 var RockComponent = preload("res://Scenes/RPSCompnents/rockcomponent.tscn")
 var PaperComponent = preload("res://Scenes/RPSCompnents/papercomponent.tscn")
 var ScissorsComponent = preload("res://Scenes/RPSCompnents/scissorscomponent.tscn")
-var p1_submitted: Object = null
-var cpu_submitted: Object = null
+var p1_submitted
+var cpu_submitted
+var winner
 
-@onready var cpu = %CPU
-@onready var p1 = %Player
+var RPS_dictionary: Dictionary = {
+	"Rock": ["Scissors"],
+	"Scissors": ["Paper"],
+	"Paper": ["Rock"]
+	}
+
+
+
+
 func _ready() -> void:
-	%Submit.pressed.connect(_on_submit_pressed.bind(p1))
-	
-	
-func _on_submit_pressed(p1obj: Object):
-	p1_submitted = p1obj.selected
-	print("Player: " + str(p1_submitted))
-	CPU_wake_up()
+	%Submit.pressed.connect(_on_submit_pressed.bind())
 
-func CPU_wake_up():
-	emit_signal("player_submitted")
-	cpu_submitted = cpu.selected
+	
+func _on_submit_pressed():
+	
+	get_tree().call_group("All_Nodes_Caller", "_choose_option")
+
+	calculate_RPS_submissions()
+	get_tree().call_group("All_Nodes_Caller", "_clear_selected")
+	
+	
+func calculate_RPS_submissions():
+	var All_Nodes_Caller_nodes = get_tree().get_nodes_in_group("All_Nodes_Caller")
+	for node in All_Nodes_Caller_nodes:
+		if node.get("is_player") != null:
+			if node.selected == "":
+				print("Need to select an option.")
+				return
+			elif node.is_player:
+				p1_submitted = node.selected
+			else:
+				cpu_submitted = node.selected
+	print("Player: " + str(p1_submitted))
 	print("CPU: " + str(cpu_submitted))
+	#replace with functions that display tie, win or loss anim.	
+	if p1_submitted == cpu_submitted:
+		print("--Tie, No Wins")
+		return 
+	if cpu_submitted in RPS_dictionary[p1_submitted]:
+		print("--Player Wins")
+		return 
+	else:
+		print("--Cpu Wins")
+		return 
---
+++ Scripts/rps_logic.gd
@@ -0,0 +1,39 @@
+extends HBoxContainer
+var selected = ""
+var list_of_available_RPS = []
+@export var is_player = true
+
+
+func _ready() -> void:
+	var GameMaster = get_tree().get_first_node_in_group("GM")
+	var rock_instance = GameMaster.RockComponent.instantiate()
+	var paper_instance = GameMaster.PaperComponent.instantiate()
+	var scissors_instance = GameMaster.ScissorsComponent.instantiate()
+	var empty_style = StyleBoxEmpty.new()
+	
+	add_child(rock_instance)
+	add_child(paper_instance)
+	add_child(scissors_instance)
+	
+	#List for easy access to components for upgrades/changes, each binded
+	for child in get_children():
+		list_of_available_RPS.append(child)
+		if is_player:
+			list_of_available_RPS.back().pressed.connect(_PLAYER_select.bind(child))
+		else:
+			list_of_available_RPS.back().disabled = true
+			list_of_available_RPS.back().add_theme_stylebox_override("focus", empty_style)
+
+
+
+func _choose_option():
+	if !is_player:
+		selected = list_of_available_RPS[randi() % list_of_available_RPS.size()].name
+
+
+func _clear_selected():
+	selected = ""
+
+
+func _PLAYER_select(child):
+	selected = child.name 
